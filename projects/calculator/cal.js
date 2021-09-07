f4fbb11b744faa5f19c1381c2fcf37395d78f80a
Mahima Raghuwanshi <mahimaraghu22@gmail.com>
2021-09-07 21:26:51 +0530

    key stroke added

--- projects/calculator/cal.js
+++ projects/calculator/cal.js
@@ -3,7 +3,7 @@ document.querySelectorAll('.calc_btn').forEach(item => {
     item.addEventListener('click', e => {
         let data = document.getElementById('display').innerText
 
-        if (data === "Invalid Expression" || (JSON.stringify(logs) === (JSON.stringify(data.split(','))))) {
+        if (data === "Invalid Expression" || data === "Infinity" || (JSON.stringify(logs) === (JSON.stringify(data.split(','))))) {
             document.getElementById('display').innerText = e.target.dataset.val;
         } else {
             document.getElementById('display').innerText += e.target.dataset.val;
@@ -15,7 +15,7 @@ document.getElementById('eql').addEventListener('click', () => {
 
 
         let res = eval(document.getElementById('display').innerText);
-        if (res) {
+        if (res != "Infinity") {
             let h = document.getElementById('display').innerText + "=" + res;
             logs.push(h);
         }
@@ -34,7 +34,7 @@ document.getElementById('AC').addEventListener('click', () => {
 
 document.getElementById('DEL').addEventListener('click', () => {
     let data = document.getElementById('display').innerText
-    if (data === "Invalid Expression") {
+    if (data === "Invalid Expression" || data === "Infinity") {
         document.getElementById('display').innerText = "";
     } else {
         document.getElementById('display').innerText = data.slice(0, -1);
@@ -52,4 +52,26 @@ document.getElementById('history').addEventListener('click', () => {
 
     }
 });
+window.addEventListener('keyup', (e) => {
+    const acceptedKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '*', '/', '%', '(', ')'];
+    // console.log(e)
+    // Add to Display
+    if (acceptedKeys.includes(e.key)) {
+        document.getElementById('display').innerText = document.getElementById('display').innerText + e.key;
+    }
+    // Clear Display
+    if (e.key === 'c') {
+        document.getElementById('display').innerText = ""
+    }
+    // Delete from Display
+    if (e.key === 'Backspace' || e.key === 'Delete') {
+        document.getElementById('display').innerText = document.getElementById('display').innerText.slice(0, -1)
+    }
+
+    // Evaluate
+    if (e.key === 'Enter' || e.key === '=') {
+        document.getElementById('display').innerText = eval(document.getElementById('display').innerText)
+    }
+
+})
 logs = [];
