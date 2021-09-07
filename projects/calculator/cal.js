9c77b71a86afa1ebd0f0b9d6977b7a6277c29384
Mahima Raghuwanshi <mahimaraghu22@gmail.com>
2021-09-07 15:48:46 +0530

    update

--- projects/calculator/cal.js
+++ projects/calculator/cal.js
@@ -1,35 +1,4 @@
-// =========================================================
-// Warn if overriding existing method
-if (Array.prototype.equals)
-    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-// attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = function(array) {
-        // if the other array is a falsy value, return
-        if (!array)
-            return false;
-
-        // compare lengths - can save a lot of time 
-        if (this.length != array.length)
-            return false;
-
-        for (var i = 0, l = this.length; i < l; i++) {
-            // Check if we have nested arrays
-            if (this[i] instanceof Array && array[i] instanceof Array) {
-                // recurse into the nested arrays
-                if (!this[i].equals(array[i]))
-                    return false;
-            } else if (this[i] != array[i]) {
-                // Warning - two different object instances will never be equal: {x:20} != {x:20}
-                return false;
-            }
-        }
-        return true;
-    }
-    // Hide method from for-in loops
-Object.defineProperty(Array.prototype, "equals", { enumerable: false });
-// =========================================================
 let logs = [];
-let spl = "";
 document.querySelectorAll('.calc_btn').forEach(item => {
     item.addEventListener('click', e => {
         let data = document.getElementById('display').innerText
@@ -59,7 +28,6 @@ document.getElementById('eql').addEventListener('click', () => {
         document.getElementById('display').innerText = "Invalid Expression";
     }
 });
-console.log(logs);
 document.getElementById('AC').addEventListener('click', () => {
     document.getElementById('display').innerText = '';
 });
