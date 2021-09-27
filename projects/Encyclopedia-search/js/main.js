8aeedb8e6b76c2db7a61f30c65ce7999bfc4f375
Mahima Raghuwanshi <mahimaraghu22@gmail.com>
2021-09-27 22:45:43 +0530

    code updated

--- projects/Encyclopedia-search/js/main.js
+++ projects/Encyclopedia-search/js/main.js
@@ -3,12 +3,16 @@ let species;
 function getSpecies() {
     let s = document.querySelector('#search1');
     if (s.value) {
-        $('.container').addClass('active');
+        $('.container-fluid').addClass('active');
         const results = species.filter(i => i.species.toLowerCase().includes(s.value.toLowerCase()));
+        document.querySelector(".rescount").innerHTML = `<h3>(${results.length} Results Found)</h3>`;
+
         updateUi(results)
     } else {
         document.querySelector(".resultbox").innerHTML = `<h3>No Results Found...</h3>`;
-        $('.container').removeClass('active');
+        document.querySelector(".rescount").innerHTML = ``;
+
+        $('.container-fluid').removeClass('active');
     }
 }
 // array
@@ -30,7 +34,7 @@ function updateUi(r) {
                     <figure class="card-img-top">
                     <img src="${i.image}" alt="${i.species}">
                     </figure>
-                    <div class="card-header">${i.species}</div>
+                    <div class="card-header"><strong>${i.species}</strong></div>
                     <div class="card-body">
                     <p>Class: ${i.class}</p>
                     <p>Mostly Found in Region: ${i.region}</p>
