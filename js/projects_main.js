a21614bc7cf5e8ba7bab979d55e0ee429ce9a4e3
Mahima Raghuwanshi <mahimaraghu22@gmail.com>
2021-10-20 00:24:34 +0530

    Update 2.0.1

--- js/projects_main.js
+++ js/projects_main.js
@@ -9,16 +9,32 @@ fetch("../data/projects.json", {
     .then(res => res.json())
     .then(data => {
         for (let i of data) {
-            ProjectHTML += `<div class="card">
-                <figure class="card-img-top">
-                <img src="${i.imgLink}" alt="${i.title}">
-                </figure>
-                <div class="card-body">
-                    <h5 class="card-title">${i.title}</h5>
-                    <p class="card-text">${i.desc}</p>
-                    <a href="${i.demoLink}" target="_blank" class="btn btn-primary ">DEMO</a>
-                </div>
-            </div>`
+            ProjectHTML += ` <div class="proj-card">
+                    <div class="proj-card__side proj-card__side--front">
+                        <div class="proj-card__picture" style="background-image: linear-gradient(to right bottom, #003B36, #012622), url(${i.imgLink})">
+                            &nbsp;
+                        </div>
+                        <h4 class="proj-card__heading">
+                            <span class="proj-card__heading-span">${i.title}</span>
+                        </h4>
+                        <div class="proj-card__details">
+                            <div class="meta">
+                            <i class="far fa-calendar-alt"></i>
+                            ${i.date}
+                            </div>
+                            <div class="meta">
+                            <i class="fas fa-bookmark"></i>
+                            <span>${i.tag}</span>
+                            </div>
+                            <p>${i.desc}</p>
+                        </div>
+                    </div>
+                    <div class="proj-card__side proj-card__side--back">
+                        <div class="proj-card__cta">
+                            <a href="${i.demoLink}" target="_blank">See Live!</a>
+                        </div>
+                    </div>
+                </div>`
         }
         project.innerHTML = ProjectHTML
     })
